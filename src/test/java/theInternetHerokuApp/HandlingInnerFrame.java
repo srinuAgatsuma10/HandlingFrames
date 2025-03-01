6dd1621f7cd84cbc88bdb8a66fe05e3d1448ed1b
Giridhara Srinivas <ggsrinivascm035@gmail.com>
2025-03-01 21:58:04 +0530

    Inner frame class updated

---
+++ src/test/java/theInternetHerokuApp/HandlingInnerFrame.java
@@ -0,0 +1,47 @@
+package theInternetHerokuApp;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class HandlingInnerFrame {
+	public static WebDriver driver;
+	WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+	@BeforeClass
+	public void setUp() {
+		driver = new ChromeDriver();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("https://the-internet.herokuapp.com/iframe");
+		// driver.manage().window().maximize();
+	}
+
+	@Test
+	public void nestedFrames() {
+		// Close the popUp window
+		driver.findElement(By.xpath("//div[@aria-label='Close']//*[name()='svg']")).click();
+
+		// Switch to Inner Frame
+		WebElement textAreaFrame = driver.findElement(By.xpath("//iframe[@id='mce_0_ifr']"));
+		driver.switchTo().frame(textAreaFrame);
+		// Accessing text in frame
+		String text = driver.findElement(By.xpath("//body[@class=\"mce-content-body mce-content-readonly\"]//p")).getText();
+
+		Assert.assertEquals(text, "Your content goes here.");
+
+	}
+
+	@AfterClass
+	public void tearDown() {
+		driver.quit();
+	}
+}
