d33250371537beabeb4583efc2e54d29006c2804
Giridhara Srinivas <ggsrinivascm035@gmail.com>
2025-03-30 19:12:31 +0530

    NestedFrmaes and Final Commit

--- src/test/java/demoAutomationTesting/HandlingNested_InnerFrame.java
+++ src/test/java/demoAutomationTesting/HandlingNested_InnerFrame.java
@@ -2,15 +2,17 @@ package demoAutomationTesting;
 
 import java.time.Duration;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class HandlingNested_InnerFrame {
-WebDriver driver;
-	
+	WebDriver driver;
+
 	@BeforeClass
 	public void setUp() {
 		driver = new ChromeDriver();
@@ -19,12 +21,21 @@ WebDriver driver;
 		driver.get("https://demo.automationtesting.in/Frames.html");
 		driver.manage().window().maximize();
 	}
-	
+
 	@Test
 	public void nestedInnerFrame() {
-		
+		driver.findElement(By.xpath("//a[normalize-space()='Iframe with in an Iframe']")).click();
+		WebElement frame1 = driver.findElement(By.xpath("//iframe[@src='MultipleFrames.html']"));
+		driver.switchTo().frame(frame1);
+		String frame1Text = driver.findElement(By.xpath("//h5[normalize-space()='Nested iFrames']")).getText();
+		System.out.println("You are in : " + frame1Text);
+		WebElement frame2 = driver
+				.findElement(By.xpath("//iframe[normalize-space()='<p>Your browser does not support iframes.</p>']"));
+		driver.switchTo().frame(frame2);
+		driver.findElement(By.xpath("//input[@type='text']")).sendKeys("Monkey D Luffy");
+
 	}
-	
+
 	@AfterClass
 	public void tearDown() {
 		driver.quit();
--- src/test/java/demoAutomationTesting/HandlingSingle_InnerFrame.java
+++ src/test/java/demoAutomationTesting/HandlingSingle_InnerFrame.java
@@ -11,9 +11,9 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class HandlingSingle_InnerFrame {
-	
+
 	WebDriver driver;
-	
+
 	@BeforeClass
 	public void setUp() {
 		driver = new ChromeDriver();
@@ -22,17 +22,17 @@ public class HandlingSingle_InnerFrame {
 		driver.get("https://demo.automationtesting.in/Frames.html");
 		driver.manage().window().maximize();
 	}
-	
+
 	@Test
 	public void singleInnerFrame() {
 		WebElement singleFrame = driver.findElement(By.xpath("//iframe[@id='singleframe']"));
 		driver.switchTo().frame(singleFrame);
 		String frameName = driver.findElement(By.xpath("//h5[normalize-space()='iFrame Demo']")).getText();
-		System.out.println("This is : "+frameName);
+		System.out.println("This is : " + frameName);
 		driver.findElement(By.xpath("//input[@type='text']")).sendKeys("Monkey D Luffy");
-		
+
 	}
-	
+
 	@AfterClass
 	public void tearDown() {
 		driver.quit();
