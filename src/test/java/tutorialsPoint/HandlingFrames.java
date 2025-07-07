3b6b5ade4b138325b7857e946d1ca1b810b26593
Giridhara Srinivas <ggsrinivascm035@gmail.com>
2025-07-07 10:04:48 +0530

    First Test Case

--- src/test/java/tutorialsPoint/HandlingFrames.java
+++ src/test/java/tutorialsPoint/HandlingFrames.java
@@ -1,8 +1,13 @@
 package tutorialsPoint;
 
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -17,16 +22,27 @@ public class HandlingFrames {
 		driver = new ChromeDriver();
 		driver.manage().deleteAllCookies();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-		driver.get("");
+		driver.get("https://www.tutorialspoint.com/selenium/practice/frames.php");
 		driver.manage().window().maximize();
 	}
 
 	@Test(priority = 1)
 	public void innerFrame1() {
+		WebElement Iframe1 = driver.findElement(By.xpath("//body//main//iframe[1]"));
+		driver.switchTo().frame(Iframe1);
 
+		driver.findElement(By.xpath("//a[@title='back to Selenium Tutorial']")).click();
+		Set<String> newTab = driver.getWindowHandles();
+		List<String> tabs = new ArrayList(newTab);
+		String parentTab = tabs.get(0);
+		String tutorialTab = tabs.get(1);
+
+		driver.switchTo().window(tutorialTab);
+		System.out.println("New Tab Name : " + driver.getTitle());
+		driver.switchTo().window(parentTab);
 	}
 
-	@Test(priority = 2)
+//	@Test(priority = 2)
 	public void innerFrame2() {
 
 	}
